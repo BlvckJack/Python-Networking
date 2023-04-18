71d65e817cffabf8b01f63e65af936ee59b60128
BlvckJack <ogonofidel@gmail.com>
2023-04-18 08:04:49 -0400

    Banner grabbing using SSH

--- ssh_connection.py
+++ ssh_connection.py
@@ -8,10 +8,15 @@ socket.setdefaulttimeout(2)
 s =  socket.socket()
 
 # Using the connect() method, we attempt to make a network connection to another IP Addr, using default SSH Port(22).
-s.connect(("10.0.2.16", 22))
+try:
+    s.connect(("10.0.2.16", 22))
 
 # recv() method reads the next 1024 bytes on the socket. The results are stored to variable 'ans' and printed. (i.e) -> print(ans)
-ans = s.recv(1024)
+    ans = s.recv(1024)
+    print("Server -> "), ans
+
+except Exception, e:
+    print("[-] Error = "), e
 
 # Checking which Version of SSH the Ip Addr is using and if the SSH Version is vulnerable.
 if ("SSH-2.0-OpenSSH" in ans):
