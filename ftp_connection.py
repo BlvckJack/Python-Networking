edc57602b7b3a8f888a387bd57691cf94add8187
BlvckJack <ogonofidel@gmail.com>
2023-04-18 08:05:52 -0400

    Banner grabbing using FTP

--- ftp_connection.py
+++ ftp_connection.py
@@ -6,20 +6,24 @@ socket.setdefaulttimeout(2)
 s = socket.socket()
 
 # Using the connect() method, we attempt to make a network connection to another IP Addr, using default FTP Port(21)
-s.connect(("10.0.2.16", 21))
+try:
+    s.connect(("10.0.2.16", 21))
+    ans = s.recv(1024)
+    print("Server ->"), ans
 
-ans = s.recv(1024)
+except Exception, e:
+    print("[-] Error = "), e
 
 # Checking which Version of FTP the Ip Addr is using and if the FTP Version is vulnerable.
 
 if ("vsFTPd 2.3.4" in ans):
-    print(" [+] vsFTPD 2.3.4 FTP Server is vulnerable")
+    print("[+] vsFTPD 2.3.4 FTP Server is vulnerable")
 elif("FreeFloat FTP Server" in ans):
-    print(" [+] FreeFloat FTP Server is vulnerable.")
+    print("[+] FreeFloat FTP Server is vulnerable.")
 elif("Ability FTP Server" in ans):
-    print(" [+]  Ability FTP Server is vulnerable." )
+    print("[+]  Ability FTP Server is vulnerable." )
 else:
-    print(" [-] FTP Server is not vulnerable.")
+    print("[-] FTP Server is not vulnerable.")
 
 
 
